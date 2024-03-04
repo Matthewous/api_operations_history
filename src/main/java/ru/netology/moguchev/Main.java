4c555c4b282ff2c2e8ae1e3588e9ed63037fe669
Matthewous <moguch98@mail.ru>
2024-03-04 22:21:05 +0300

    внедрение цикла while

--- src/main/java/ru/netology/moguchev/Main.java
+++ src/main/java/ru/netology/moguchev/Main.java
@@ -5,29 +5,35 @@ import java.time.LocalDate;
 class FinancialTransaction {
 
     public static void main(String[] args) {
-        // Создание переменных
-        int transactionId;
-        String transactionType;
-        double amount;
-        LocalDate transactionDate;
-
-        // Создание объекта Scanner
         Scanner scanner = new Scanner(System.in);
 
-        // Заполнение переменных пользователем
-//        System.out.println("Введите идентификатор транзакции:");
-//        transactionId = scanner.nextInt();
+        while (true) {
+            // Создание переменных
+            String transactionType;
+            double amount;
+            LocalDate transactionDate;
 
-        System.out.println("Введите тип транзакции:");
-        transactionType = scanner.nextLine();
+            System.out.println("Введите тип транзакции:");
+            transactionType = scanner.nextLine();
 
-        System.out.println("Введите сумму транзакции:");
-        amount = scanner.nextDouble();
+            System.out.println("Введите сумму транзакции:");
+            amount = scanner.nextDouble();
 
-        System.out.println("Введите дату транзакции (в формате гггг-мм-дд):");
-        transactionDate = LocalDate.parse(scanner.nextLine());
+            System.out.println("Введите дату транзакции (в формате гггг-мм-дд):");
+            scanner.nextLine(); // очистка буфера после ввода числа
+            transactionDate = LocalDate.parse(scanner.nextLine());
 
-        // Вывод информации в консоль
-        System.out.println("Финансовая транзакция: " + " " + transactionType + " " + amount + " " + transactionDate);
+            // Вывод информации в консоль
+            System.out.println("Финансовая транзакция: " + " " + transactionType + " " + amount + " " + transactionDate);
+
+            System.out.println("Продолжить ввод новой транзакции? (да/нет)");
+            String answer = scanner.nextLine();
+
+            if (!answer.equalsIgnoreCase("да")) {
+                break; // завершение программы, если пользователь не хочет продолжать
+            }
+        }
+
+        System.out.println("Программа завершена.");
     }
 }
